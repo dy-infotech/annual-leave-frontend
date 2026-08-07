081e27114445b96c429bb6b842d90826de679248
박묘성 <aytjd333@naver.com>
2026-08-07 10:00:03 +0900

    Merge origin/develop into release

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -4,11 +4,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class ApiConfig {
   static String get baseUrl {
     //if (kIsWeb) {
-<<<<<<< HEAD
     // return 'http://localhost:8080';
-=======
-    //return 'http://localhost:8080';
->>>>>>> origin/develop
     return 'https://app.dyinfotech.com';
     //}
   }
