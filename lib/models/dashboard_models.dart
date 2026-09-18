271b936b8d95e82ba190025d4430ab8e0d9f2cc9
solebloom <48436057+solebloom@users.noreply.github.com>
2026-09-18 12:40:51 +0900

    feat: parse personal leave period

--- lib/models/dashboard_models.dart
+++ lib/models/dashboard_models.dart
@@ -1,3 +1,17 @@
+class LeavePeriod {
+  final DateTime startDate;
+  final DateTime endDate;
+
+  LeavePeriod({required this.startDate, required this.endDate});
+
+  factory LeavePeriod.fromJson(Map<String, dynamic> json) {
+    return LeavePeriod(
+      startDate: DateTime.parse(json['startDate']),
+      endDate: DateTime.parse(json['endDate']),
+    );
+  }
+}
+
 class LeaveInfo {
   final double totalLeaveDays;
   final double usedLeaveDays;
@@ -39,11 +53,13 @@ class LeaveRequestSummary {
 }
 
 class DashboardData {
+  final LeavePeriod myLeavePeriod;
   final LeaveInfo myLeaveInfo;
   final LeaveRequestSummary myRequestSummary;
   final LeaveRequestSummary? allEmployeeRequestSummary; // ADMIN 권한을 가진 사용자만 값 존재
 
   DashboardData({
+    required this.myLeavePeriod,
     required this.myLeaveInfo,
     required this.myRequestSummary,
     this.allEmployeeRequestSummary,
@@ -51,6 +67,7 @@ class DashboardData {
 
   factory DashboardData.fromJson(Map<String, dynamic> json) {
     return DashboardData(
+      myLeavePeriod: LeavePeriod.fromJson(json['myLeavePeriod']),
       myLeaveInfo: LeaveInfo.fromJson(json['myLeaveInfoResponse']),
       myRequestSummary: LeaveRequestSummary.fromJson(json['myRequestSummary']),
       allEmployeeRequestSummary: json['allEmployeeRequestSummary'] != null
